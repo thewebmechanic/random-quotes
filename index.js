21937eed658dbe5e2b5c28e043c06aadbfc26720
thewebmechanic <alexthewebmechanic@gmail.com>
2022-11-21 17:03:29 +0200

    fully implemented random generating quote

---
+++ index.js
@@ -0,0 +1,56 @@
+const generateButton = document.getElementById('generate-button')
+const quote = document.querySelector('.quote')
+const author = document.querySelector('.author')
+
+const quotes = [
+    {
+        quote: "Be yourself; everyone else is already taken.",
+        author: "Oscar Wilde"
+    }, 
+    {
+        quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
+        author: "Marilyn Monroe"
+    },
+    {
+        quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+        author: " Life"
+    },
+    {
+        quote:
+            "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+        author: "John Kenneth Galbraith"
+    },
+    {
+        quote:
+            "God save me from my friends. I can protect myself from my enemies.",
+        author: "Claude Louis Hector de Villars "
+    },
+    {
+        quote: "The price of anything is the amount of life you exchange for it.",
+        author: "David Thoreau"
+    },
+    {
+        quote:
+            "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+        author: "Charles Lindbergh"
+    },
+    {
+        quote:
+            "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+        author: " Tyne Daly"
+    }
+]
+
+window.addEventListener('load', loadQuote)
+generateButton.addEventListener('click', loadQuote)
+
+function loadQuote () {
+    let random = Math.floor(Math.random() * quotes.length)
+    // console.log(random)
+    quote.textContent = quotes[random].quote
+    author.textContent = quotes[random].author
+}
